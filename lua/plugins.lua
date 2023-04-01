fbb5aad2d2c02b7a884414e59a5b2516064bf1bb
funnsam <funnsam8@gmail.com>
2023-04-01 07:47:05 +0000

    a

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,14 +4,10 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-lualine/lualine.nvim',
-  		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-	}
+	use 'nvim-lualine/lualine.nvim'
 	use 'nvim-tree/nvim-web-devicons'
-	use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'}
+	use 'romgrk/barbar.nvim'
 	use 'navarasu/onedark.nvim'
 	
 	use {'neoclide/coc.nvim', branch = 'release'}
