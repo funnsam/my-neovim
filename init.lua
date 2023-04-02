d05624e554800659891e67228576ccc4f1f96dc9
funnsam <funnsam8@gmail.com>
2023-04-02 11:58:07 +0000

    a

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@ vim.opt.relativenumber = true
 vim.opt.swapfile = false
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.completeopt = {"menuone", "noselect", "noinsert"}
 vim.opt.shortmess = vim.opt.shortmess + {c = true}
-vim.api.nvim_set_option('updatetime', 300) 
+vim.api.nvim_set_option("updatetime", 300) 
 
-require('plugins')
+require("plugins")
 
 vim.cmd([[
 	augroup packer_user_config
@@ -21,23 +21,26 @@ vim.cmd([[
 	autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
 
+-- Depth bar
+require("barbecue").setup()
+
 -- Status bar
-local a = { left = '', right = '' }
-local b = { left = '', right = '' }
-require('lualine').setup {
+local a = { left = "", right = "" }
+local b = { left = "", right = "" }
+require("lualine").setup {
   	options = {
-    	theme = 'onedark',
+    	theme = "onedark",
 		globalstatus = true,
-    	component_separators = '|',
+    	component_separators = "|",
 	    section_separators = a,
   	},
   	sections = {
-   		lualine_a = {{'mode' 	, separator = b}},
-    	lualine_b = {{'branch' 	}},
-   		lualine_c = {{'filename', 'diagnostics'}},
-    	lualine_x = {{'diff', 'filetype'}},
-    	lualine_y = {{'progress'}},
-    	lualine_z = {{'location', separator = b}}
+   		lualine_a = {{"mode" 	, separator = b}},
+    	lualine_b = {{"branch" 	}},
+   		lualine_c = {{"filename"}, {"diagnostics"}},
+    	lualine_x = {{"diff"}, {"filetype"}},
+    	lualine_y = {{"progress"}},
+    	lualine_z = {{"location", separator = b}}
   	},
 }
 
@@ -45,44 +48,44 @@ require('lualine').setup {
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
-map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
-map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
+map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
+map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
+map("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
+map("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
+map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
+map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
+map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
+map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
 
 -- File list
-require('nvim-tree').setup()
-map('n', '<C-b>', '<Cmd>NvimTreeToggle<CR>', opts)
-vim.cmd('NvimTreeOpen')
+require("nvim-tree").setup()
+map("n", "<C-b>", "<Cmd>NvimTreeToggle<CR>", opts)
+vim.cmd("NvimTreeOpen")
 
 -- Theme
-require('onedark').setup = {
+require("onedark").setup = {
 	code_style = {
-		functions = 'bold',
-		comments = 'none'
+		functions = "bold",
+		comments = "none"
 	}
 }
-require('onedark').load()
+require("onedark").load()
 
 -- Shade
-require('shade').setup({
+require("shade").setup({
   	overlay_opacity = 35
 })
 
@@ -91,36 +94,36 @@ require('shade').setup({
 local keyset = vim.keymap.set
 -- Autocomplete
 function _G.check_back_space()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+    local col = vim.fn.col(".") - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
 end
 
 -- Use Tab for trigger completion with characters ahead and navigate
--- NOTE: There's always a completion item selected by default, you may want to enable
+-- NOTE: There"s always a completion item selected by default, you may want to enable
 -- no select by setting `"suggest.noselect": true` in your configuration file
--- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
+-- NOTE: Use command ":verbose imap <tab>" to make sure Tab is not mapped by
 -- other plugins before putting this into your config
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? \"<TAB>\" : coc#refresh()", opts)
 keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 -- Use K to show documentation in preview window
 function _G.show_docs()
-    local cw = vim.fn.expand('<cword>')
-    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
-        vim.api.nvim_command('h ' .. cw)
-    elseif vim.api.nvim_eval('coc#rpc#ready()') then
-        vim.fn.CocActionAsync('doHover')
+    local cw = vim.fn.expand("<cword>")
+    if vim.fn.index({"vim", "help"}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command("h " .. cw)
+    elseif vim.api.nvim_eval("coc#rpc#ready()") then
+        vim.fn.CocActionAsync("doHover")
     else
-        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+        vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
     end
 end
-keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+keyset("n", "K", "<CMD>lua _G.show_docs()<CR>", {silent = true})
 
 -- Highlight the symbol and its references on a CursorHold event(cursor is idle)
 vim.api.nvim_create_augroup("CocGroup", {})
 vim.api.nvim_create_autocmd("CursorHold", {
     group = "CocGroup",
-    command = "silent call CocActionAsync('highlight')",
+    command = "silent call CocActionAsync(\"highlight\")",
     desc = "Highlight symbol under cursor on CursorHold"
 })
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,18 +1,22 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'nvim-lualine/lualine.nvim'
-	use 'nvim-tree/nvim-web-devicons'
-	use 'romgrk/barbar.nvim'
-	use 'navarasu/onedark.nvim'
+return require("packer").startup(function(use)
+	use "wbthomason/packer.nvim"
+	use "nvim-lualine/lualine.nvim"
+	use "nvim-tree/nvim-web-devicons"
+	use "romgrk/barbar.nvim"
+	use "navarasu/onedark.nvim"
 	
-	use {'neoclide/coc.nvim', branch = 'release'}
+	use {"neoclide/coc.nvim", branch = "release"}
 
-	use 'nvim-tree/nvim-tree.lua'
+	use "nvim-tree/nvim-tree.lua"
 
-	use 'sunjon/shade.nvim'
+	use "sunjon/shade.nvim"
+
+	use {
+  		"utilyre/barbecue.nvim",
+  		tag = "*",
+  		requires = {
+    		"SmiteshP/nvim-navic",
+    		"nvim-tree/nvim-web-devicons", -- optional dependency
+ 		},
+	}
 end)
