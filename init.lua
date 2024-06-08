517eb8b652f09474e376e21749f756f15cc24b94
funnsam <funnsam8@gmail.com>
2024-06-08 10:11:28 +0000

    h

--- init.lua
+++ init.lua
@@ -140,21 +140,23 @@ cmp.setup({
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-h>"] = cmp.mapping.open_docs(),
+        ["<C-H>"] = cmp.mapping.close_docs(),
+        ["<Esc>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' },
+        { name = "nvim_lsp" },
+        { name = "vsnip" },
     }, {
-        { name = 'buffer' },
+        { name = "buffer" },
     })
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- LSPs
 require("mason").setup()
